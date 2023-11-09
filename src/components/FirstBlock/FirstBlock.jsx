86e69027dc981d693257a8d1296c79e2a67cecb5
kryvko.ov <kryvko.ov@novaposhta.ua>
2023-11-09 14:48:23 +0200

    FirstBlock

--- src/components/FirstBlock/FirstBlock.jsx
+++ src/components/FirstBlock/FirstBlock.jsx
@@ -8,8 +8,8 @@ const FirstBlock = () => {
      <div className="container my-5 py-5 order" id="order">
          <div className="row align-items-center">
              <div className="col-12 col-md-7">
-                 <h1 className="mb-3">Shopping - маркетплейс польських товарів в Україні</h1>
-                 <p className="fs-6">
+                 <h1 className="mb-3" style={{"font-size":"52px"}}>Shopping - маркетплейс польських товарів в Україні</h1>
+                 <p className="fs-5">
                      Ставайте продавцем в найбільшому українському додатку, а логістику, технічну підтримку і знання
                      українського e-commerce ми беремо на себе. </p>
                  <Btn type="success" text="Стати партнером"/>
