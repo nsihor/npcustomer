df34b8a80d3ff7d39159e8f615892e6a5049925a
nsihor <nsihor@gmail.com>
2023-11-15 14:08:20 +0200

    update PasswordInput

--- src/components/PasswordInput/PasswordInput.jsx
+++ src/components/PasswordInput/PasswordInput.jsx
@@ -4,17 +4,17 @@ import css from "./PasswordInput.module.scss"
 import sprite from "../../images/sprite.svg"
 import {useState} from "react";
 
-const PasswordInput = ({handleSetPassword, password, inputClass}) => {
+const PasswordInput = ({handleSetPassword, password, inputClass, name='password'}) => {
     const [isPasswordVisible, setIsPasswordVisible] = useState(false);
     const switchIsPasswordVisible = () => setIsPasswordVisible(prevState => !prevState);
 
     return (
         <div className={css.wrapper}>
             <Field
-                name='password'
+                name={name}
                 type={isPasswordVisible ? 'text' : 'password'}
                 placeholder='Пароль'
-                className={clsx(css.input, css[inputClass], 'rounded border-0 mb-3 py-3 px-2')}
+                className={clsx(css.input, css[inputClass], 'rounded border-0 mb-3 py-3 px-2 w-100')}
                 value={password}
                 onChange={handleSetPassword}
             />
