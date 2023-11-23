af92357fa8ae7a456ab049630a88f7591a200f88
nsihor <nsihor@gmail.com>
2023-11-23 22:31:14 +0200

    add redirect from finishedRegistrationModal

--- src/components/modals/BasicModalWindow/BasicModalWindow.jsx
+++ src/components/modals/BasicModalWindow/BasicModalWindow.jsx
@@ -1,51 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 import sprite from '../../../images/sprite.svg';
 import css from './BasicModalWindow.module.scss';
 import ReactDOM from 'react-dom'
+import {useNavigate} from "react-router-dom";
 
-const BasicModalWindow = ({ children, onClose}) => {
-  useEffect(() => {
-    const handleKeyDown = (e) => {
-      if (e.code === 'Escape') {
-        onClose();
-      }
+const BasicModalWindow = ({children, onClose, navigateTo}) => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            if (e.code === 'Escape') {
+                onClose();
+                navigateTo && navigate(navigateTo, {replace: true});
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [navigate, navigateTo, onClose]);
+
+    const handleBackdropClick = (e) => {
+        if (e.target === e.currentTarget) {
+            onClose();
+            navigateTo && navigate(navigateTo, {replace: true})
+        }
     };
-
-    window.addEventListener('keydown', handleKeyDown);
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [onClose]);
-
-  const handleBackdropClick = (e) => {
-    if (e.target === e.currentTarget) {
-      onClose();
-    }
-  };
-  return ReactDOM.createPortal(
-      <div className={css.backdrop} onClick={handleBackdropClick}>
-        <div className={css.modal}>
-          <button
-              type='button'
-              className={css.modalCloseBtn}
-              onClick={onClose}
-          >
-            <svg className={css.modalCloseIcon}>
-              <use href={sprite + '#icon-close'}></use>
-            </svg>
-          </button>
-          {children}
-        </div>
-      </div>,
-      document.querySelector("#modal")
-  )
+    return ReactDOM.createPortal(
+        <div className={css.backdrop} onClick={handleBackdropClick}>
+            <div className={css.modal}>
+                <button
+                    type='button'
+                    className={css.modalCloseBtn}
+                    onClick={() => {
+                        onClose();
+                        navigateTo && navigate(navigateTo, {replace: true});
+                    }}>
+                    <svg className={css.modalCloseIcon}>
+                        <use href={sprite + '#icon-close'}></use>
+                    </svg>
+                </button>
+                {children}
+            </div>
+        </div>,
+        document.querySelector("#modal")
+    )
 };
 
 export default BasicModalWindow;
 
 BasicModalWindow.propTypes = {
-  onClose: PropTypes.func.isRequired,
-  showCloseBtn: PropTypes.bool,
+    onClose: PropTypes.func.isRequired,
+    showCloseBtn: PropTypes.bool,
 };
