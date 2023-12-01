14b9e2c69b3ea08775e576c82dd88c61c7dd679a
nsihor <nsihor@gmail.com>
2023-12-01 16:18:03 +0200

    update mobile version

--- src/components/FirstBlock/FirstBlock.jsx
+++ src/components/FirstBlock/FirstBlock.jsx
@@ -1,9 +1,11 @@
 
 import React from "react";
+import {useTranslation} from "react-i18next"
+import {Link} from "react-router-dom";
+import clsx from "clsx";
 import macbook from "../../images/MainPage/macbook.png";
 import Btn from "../Btn/Btn";
-import {Link} from "react-router-dom";
-import {useTranslation} from "react-i18next"
+import css from "./FirstBlock.module.scss"
 
 const FirstBlock = () => {
     const {t} = useTranslation();
@@ -12,7 +14,7 @@ const FirstBlock = () => {
      <div className="container my-5 py-5 order" id="order">
          <div className="row align-items-center">
              <div className="col-12 col-md-7">
-                 <h1 className="mb-3" style={{"fontSize":"52px"}}>Shopping - {t("FirstBlock.title")}</h1>
+                 <h1 className={clsx(css.title, "mb-3 fw-bolder")}>Shopping - {t("FirstBlock.title")}</h1>
                  <p className="fs-5">{t("FirstBlock.text")}</p>
                  <Link to={'/registration'}  className='btn btn-light text-nowrap bg-transparent border-0 py-0 px-0'>
                      <Btn styled="success" text={t("FirstBlock.btn")}/>
--- src/components/Statistic/Statistic.jsx
+++ src/components/Statistic/Statistic.jsx
@@ -1,24 +1,45 @@
 import css from "./Statistic.module.scss";
 import sprite from "../../images/sprite.svg";
 import {useTranslation} from "react-i18next";
+import {useEffect, useState} from "react";
+import clsx from "clsx";
 
 const Statistic = () => {
     const {t} = useTranslation();
 
-    return (<div className="container-sm position-relative" id="about">
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 576); // Перевірка ширини екрану на мобільний розмір
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return (
+    <div className="container-sm position-relative" id="about">
         <div className={css.statSm}>
-            <div className="d-flex align-items-start align-items-md-center">
-                <div className="d-flex align-items-center flex-column flex-md-row">
-                    <span className={css.statSmDigit}>40+</span>
+            <div className="d-flex align-items-center flex-column flex-sm-row">
+                <div className="d-flex align-items-center me-3 me-sm-0 ps-4 ps-sm-0 text-start">
+                    <span className={clsx(css.statSmDigit, "me-3")}>40+</span>
                     <span>{t("Statistic.users")}</span>
                 </div>
-                <div className="mx-4 mt-4 mt-md-0">
-                    <svg width="17" height="40">
-                        <use href={sprite + "#icon-diagonalLines"}/>
-                    </svg>
+                <div className={clsx(css.svgWrapper, "mx-4 mt-0 mx-sm-3")}>
+                    {!isMobile ?
+                        <svg width="17" height="40">
+                            <use href={sprite + "#icon-diagonalLines"}/>
+                        </svg> :
+                        <svg className={css.mobileDiagonalLines}>
+                            <use href={sprite + "#icon-mobileDiagonalLines"}/>
+                        </svg>
+                    }
                 </div>
-                <div className="d-flex align-items-center flex-column flex-md-row">
-                    <span className={css.statSmDigit}>11+</span>
+                <div className="d-flex align-items-center ps-4 ps-sm-0 text-start">
+                    <span className={clsx(css.statSmDigit, "ms-2 ms-sm-0 me-3")}>11+</span>
                     <span>{t("Statistic.customers")}</span>
                 </div>
             </div>
