c98d2d257ed2c694f3617096088d3f2c2300a2a6
nsihor <nsihor@gmail.com>
2023-12-17 22:27:48 +0200

    update RegistrationStepsWrap.jsx

--- src/components/RegistrationSteps/RegistrationStepsWrap/RegistrationStepsWrap.jsx
+++ src/components/RegistrationSteps/RegistrationStepsWrap/RegistrationStepsWrap.jsx
@@ -27,38 +27,38 @@ const RegistrationStepsWrap = ({children, openStep}) => {
       <div className={clsx(css.registration, "d-flex flex-column flex-md-row mt-md-5")}>
         <div className={clsx(css.leftSide, "col-12 col-md-4 d-md-flex")}>
           <div className={clsx(css.stepsBlock, "row justify-content-start")}>
-            <div className={clsx(openStep >= 1 ? css.active : '', "d-flex align-items-center gap-4")}>
+            <div className={clsx(openStep >= 1 ? css.active : '', "d-flex align-items-center gap-3 gap-lg-4")}>
               <div className={clsx(css.circleStep, "d-flex row")}>
                 <span className={clsx(css.circleNumber)}>01</span>
                 <span className={clsx(css.circleStepText)}>{t("RegistrationSteps.step")}</span>
               </div>
-              <div className={clsx(css.title, "fs-5")}>{t("RegistrationSteps.authorizationStep")}</div>
+              <div className={css.title}>{t("RegistrationSteps.authorizationStep")}</div>
             </div>
-            <div className={clsx(openStep >= 2 ? css.active : '', "d-flex align-items-center gap-4")}>
+            <div className={clsx(openStep >= 2 ? css.active : '', "d-flex align-items-center gap-3 gap-lg-4")}>
               <div className={clsx(css.circleStep, "d-flex row")}>
                 <span className={clsx(css.circleNumber)}>02</span>
                 <span className={clsx(css.circleStepText)}>{t("RegistrationSteps.step")}</span>
               </div>
-              <div className={clsx(css.title, "fs-5")}>{t("RegistrationSteps.aboutStep")}</div>
+              <div className={css.title}>{t("RegistrationSteps.aboutStep")}</div>
             </div>
-            <div className={clsx(openStep >= 3 ? css.active : '', "d-flex align-items-center gap-4")}>
+            <div className={clsx(openStep >= 3 ? css.active : '', "d-flex align-items-center gap-3 gap-lg-4")}>
               <div className={clsx(css.circleStep, "d-flex row")}>
                 <span className={clsx(css.circleNumber)}>03</span>
                 <span className={clsx(css.circleStepText)}>{t("RegistrationSteps.step")}</span>
               </div>
-              <div className={clsx(css.title, "fs-5")}>{t("RegistrationSteps.addressStep")}</div>
+              <div className={css.title}>{t("RegistrationSteps.addressStep")}</div>
             </div>
-            <div className={clsx(openStep >= 4 ? css.active : '', "d-flex align-items-center gap-4")}>
+            <div className={clsx(openStep >= 4 ? css.active : '', "d-flex align-items-center gap-3 gap-lg-4")}>
               <div className={clsx(css.circleStep, "d-flex row")}>
                 <span className={clsx(css.circleNumber)}>04</span>
                 <span className={clsx(css.circleStepText)}>{t("RegistrationSteps.step")}</span>
               </div>
-              <div className={clsx(css.title, "fs-5")}>{t("RegistrationSteps.additionalStep")}</div>
+              <div className={css.title}>{t("RegistrationSteps.additionalStep")}</div>
             </div>
           </div>
         </div>
-        <div className={clsx(css.rightSide, "col-12 col-md-8")}>
-          <div className="mt-4">
+        <div className={clsx(css.rightSide, "col-12 col-md-8 mt-4 mt-md-0")}>
+          <div className="">
             {children}
           </div>
         </div>
